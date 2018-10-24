75b8e1821cdc9722e7c777361174eaf1ba5d96f8
Carrie Scott <carriescott@Carries-MacBook-Pro.local>
2018-10-24 11:47:17 -0500

    rendering all results

--- 01-Templates/zinc.js
+++ 01-Templates/zinc.js
@@ -1,39 +1,42 @@
 'use strict';
 
+function renderTemplate(templateString, data) {
+    return templateString.replace(/\{\{\s*(.*?)\s*\}\}/g,(match,p1) => data[p1]);            
+}
+
+const templateString = `<li class="user">
+<img class="user-photo" src="{{ photo }}" alt="Photo of {{ firstName }} {{ lastName }}">
+<div class="user-name">{{ firstName }} {{ lastName }}</div>
+<div class="user-location">{{ city }}, {{ state }}</div>
+<div class="user-email">{{ email }}</div>
+</li>`;
 
 (() => {
     function populateList(results) {
         console.log(results);
-
-        //This temporary to pass data to test template//
-            const myUser = {
-                photo: results[0].picture.thumbnail,
-                firstName: results[0].name.first,
-                lastName: results[0].name.last,
-                city: results[0].location.city,
-                state: results[0].location.state,
-                email: results[0].email }
-        //end of temporary object
-        console.log(myUser);
-
+                
         let container = document.getElementsByClassName('container')[0];
         let userList = document.createElement('ul');
         container.appendChild(userList);
         userList.setAttribute('class','user-list');
         userList.setAttribute('id','z-user-list');
         
-        function renderTemplate(templateString, data) {
-            return templateString.replace(/\{\{\s*(.*?)\s*\}\}/g,(match,p1) => data[p1]);            
-        }            
-            const userLi = renderTemplate(
-            `<li class="user">
-            <img class="user-photo" src="{{ photo }}" alt="Photo of {{ firstName }} {{ lastName }}">
-            <div class="user-name">{{ firstName }} {{ lastName }}</div>
-            <div class="user-location">{{ city }}, {{ state }}</div>
-            <div class="user-email">{{ email }}</div>
-            </li>`, myUser);
+        function formatResults (result) {
+            let user = {
+                photo: result.picture.large,
+                firstName: result.name.first,
+                lastName: result.name.last,
+                city: result.location.city,
+                state: result.location.state,
+                email: result.email 
+            } 
+            return user;
+        }
 
-        userList.insertAdjacentHTML("afterbegin", userLi);
+        let users = results.map( (result) => formatResults(result));
+        let list = users.map((user) => renderTemplate(templateString,user));
+
+        list.forEach( (item) => userList.insertAdjacentHTML("beforeend", item));
         
     }
 
