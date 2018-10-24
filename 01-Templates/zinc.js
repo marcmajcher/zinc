a0089145de92cd190c75d96c22342e7a0e0bd510
Carrie Scott <carriescott@Carries-MacBook-Pro.local>
2018-10-24 11:05:30 -0500

    renderTemplate function working

--- 01-Templates/zinc.js
+++ 01-Templates/zinc.js
@@ -1,10 +1,40 @@
 'use strict';
 
-/* eslint-env browser */
 
 (() => {
     function populateList(results) {
-        console.log(results); // eslint-disable-line no-console
+        console.log(results);
+
+        //This temporary to pass data to test template//
+            const myUser = {
+                photo: results[0].picture.thumbnail,
+                firstName: results[0].name.first,
+                lastName: results[0].name.last,
+                city: results[0].location.city,
+                state: results[0].location.state,
+                email: results[0].email }
+        //end of temporary object
+        console.log(myUser);
+
+        let container = document.getElementsByClassName('container')[0];
+        let userList = document.createElement('ul');
+        container.appendChild(userList);
+        userList.setAttribute('class','user-list');
+        userList.setAttribute('id','z-user-list');
+        
+        function renderTemplate(templateString, data) {
+            return templateString.replace(/\{\{\s*(.*?)\s*\}\}/g,(match,p1) => data[p1]);            
+        }            
+            const userLi = renderTemplate(
+            `<li class="user">
+            <img class="user-photo" src="{{ photo }}" alt="Photo of {{ firstName }} {{ lastName }}">
+            <div class="user-name">{{ firstName }} {{ lastName }}</div>
+            <div class="user-location">{{ city }}, {{ state }}</div>
+            <div class="user-email">{{ email }}</div>
+            </li>`, myUser);
+
+        userList.insertAdjacentHTML("afterbegin", userLi);
+        
     }
 
     function init() {
@@ -12,6 +42,6 @@
             .then(res => res.json())
             .then(json => populateList(json.results));
     }
-
+    
     document.addEventListener('DOMContentLoaded', init);
 })();
