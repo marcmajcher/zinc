449f37b001e3e3b2dc1fbe18b6922c563bb0d71a
Carrie Scott <carriescott@Carries-MacBook-Pro.local>
2018-10-26 13:07:32 -0500

    refactored part 4 of templates to improve renderTemplate function

--- 01-Templates/zinc.js
+++ 01-Templates/zinc.js
@@ -1,32 +1,33 @@
 'use strict';
 
-function unNestData (string, start) {
-    return string.split('.').reduce( (acc,curr) => {
-        return acc[curr];
-    }, start)
-}
-
-function renderTemplate (templateString, data) {
-    return templateString.replace(/\{\{\s*(.*?)\s*\}\}/g,(match,string) => unNestData(string,data));            
-}
-
 (() => {
     function populateList(results) {
         console.log(results);
-        
+
         let container = document.getElementsByClassName('container')[0];
         let userList = document.createElement('ul');
         container.appendChild(userList);
         userList.setAttribute('class','user-list');
         userList.setAttribute('id','z-user-list');
-        
-        fetch ('user.html')
-        .then((response) => {
-        return response.text();
-        })
-        .then((templateString) => {
-            let list = results.map((result) => renderTemplate(templateString, result))
-            list.forEach( (item) => userList.insertAdjacentHTML("beforeend", item));
+   
+        function renderTemplate (templateFile, user) {
+            //fetch template string 
+            return fetch(templateFile+'.html')
+            .then ((response) => response.text())
+            // passed 1 user, fill in template with data, then instert HTML
+                // we will need to unNest the data from each user result
+            .then ((templateString) => {
+                return templateString.replace(/\{\{\s*(.*?)\s*\}\}/g , (match, p1) => {
+                    //UnNest data-String from user
+                    return p1.split('.').reduce( (acc, curr) => {
+                        return acc[curr];
+                    }, user)    
+                })
+            }) 
+            .then ((ui) => userList.insertAdjacentHTML('beforeend', ui))
+        }
+        results.forEach( (user) => {
+            renderTemplate('user', user)
         })
     }
 
--- 02-Components/zinc.js
+++ 02-Components/zinc.js
@@ -1,10 +1,34 @@
 'use strict';
 
+const userData = {
+    picture: {
+        thumbnail: 'https://f4.bcbits.com/img/0001142378_10.jpg'
+    },
+    name: {
+        first: 'Jack',
+        last: 'Burton'
+    },
+    location: {
+        city: 'San Francisco',
+        state: 'CA'
+    },
+    email: 'jack.burton@example.com'
+};
+
 const Zinc = {};
 
 (() => {
     function renderComponent(element, content, userData) {
-        console.log(element, content);
+        console.log('element: ', element, '| content: ',content);
+        // element = user-item = the component we need to define/create
+        // content = user = the html string
+        // userData = userData object (the info)
+        // get html string
+        // render html using userData
+        // append html to parent element
+
+        //get parent element
+        document.getElementsByClassName('')
     }
     console.log(userData);
 
@@ -15,3 +39,4 @@ const Zinc = {};
     document.addEventListener('DOMContentLoaded', init);
 })();
 
+// npm install -g lite-server
